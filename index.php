8fc4ad34de26de74042b1756611fe2f1f28215e2
willgholton <140411676+willgholton@users.noreply.github.com>
2025-03-15 19:57:37 +0000

    test

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html lang="en">
     
 <?php
-echo "hello";
+
 function fetch_file($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $content = curl_exec($ch);
     if (curl_errno($ch)) {
-        return '/* curl error: ' . curl_error($ch) . ' */';
+        return 'curl error: ' . curl_error($ch);
     }
     curl_close($ch);
     return $content;
